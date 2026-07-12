0f80c8344b888d609cc26a454956b9d474cc01aa
abinivesh1696 <abiniveshcsea@gmail.com>
2026-07-13 01:06:59 +0530

    Dashboard added

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+function Dashboard() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container mt-5">
+        <h2>Dashboard</h2>
+        <p>Welcome to the Hospital Appointment System.</p>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
